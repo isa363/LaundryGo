60ea1d94974f2f32f6994a2e61af2dc20191a682
LNN10 <129519461+LNN10@users.noreply.github.com>
2026-04-09 22:17:58 -0400

    Update build.gradle.kts

--- laundryproject/app/build.gradle.kts
+++ laundryproject/app/build.gradle.kts
@@ -47,8 +47,12 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+// MPAndroidChart
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     //FIREBASE
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
+    implementation ("com.google.firebase:firebase-messaging")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
